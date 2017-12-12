29e349a125106e6dd16fafd99b4ce98484c2a91b
Matt Trussell <smtrussell@gmail.com>
2017-12-12 04:11:31 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -4,6 +4,15 @@
 class Student
   attr_accessor :name, :grade
   attr_reader :id
+  @@all = []
+
+
+  def initialize (name, grade, id = nil)
+    @name = name
+    @grade = grade
+
+    @@all << self
+  end
 
   def self.create_table
   end
