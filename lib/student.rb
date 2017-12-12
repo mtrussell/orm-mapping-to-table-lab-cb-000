0d490db294916d209989465294668811ccfcdd39
Matt Trussell <smtrussell@gmail.com>
2017-12-12 04:09:03 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,5 +2,29 @@ class Student
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+
+  attr_accessor :name, :grade
+  attr_reader :id
+
+  def self.create_table
+
+  end
+
+
+  def self.drop_table
+
+  end
+
+
+  def self.create
+
+  end
+
+
+  def save
+
+  end
+
+  
   
 end
